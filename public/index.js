152492e04deeb2544b267b8124e1fd0842579283
Pfaff Fabian <fabian.pfaff@infineon.com>
2025-09-07 15:29:59 +0200

    add confidence rating

--- public/index.js
+++ public/index.js
@@ -1,17 +1,29 @@
 
 const comparisons = [
     {
-        name: "image1",
-        baseline: "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/01afb14f-8316-41a4-a624-bd3b80634dfc",
-        implementation: "https://s3muccepht.infineon.com/visualverifier/candidates/a8f52d22-3efd-40fa-b9cf-2a359accfcdc/visual-artifact.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=SO6UTD7307PAV4KMB6H1%2F20250907%2Feu-muc%2Fs3%2Faws4_request&X-Amz-Date=20250907T000700Z&X-Amz-Expires=86400&X-Amz-Signature=27ac011fb285734660213fd996e935e80f36864b1b04c86f29e9519fa40f9596&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject",
+        name: "ifx-checkbox-m-checked-indeterminate",
+        baseline: "/images/checkbox/ifx-checkbox-m-checked-indeterminate_baseline.png",
+        implementation: "/images/checkbox/ifx-checkbox-m-checked-indeterminate_candidate.png",
         backgroundColor: "#ffffff"
     },
     {
-        name: "image1",
-        baseline: "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/01afb14f-8316-41a4-a624-bd3b80634dfc",
-        implementation: "https://s3muccepht.infineon.com/visualverifier/candidates/a8f52d22-3efd-40fa-b9cf-2a359accfcdc/visual-artifact.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=SO6UTD7307PAV4KMB6H1%2F20250907%2Feu-muc%2Fs3%2Faws4_request&X-Amz-Date=20250907T000700Z&X-Amz-Expires=86400&X-Amz-Signature=27ac011fb285734660213fd996e935e80f36864b1b04c86f29e9519fa40f9596&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject",
+        name: "ifx-checkbox-s-checked",
+        baseline: "/images/checkbox/ifx-checkbox-s-checked_baseline.png",
+        implementation: "/images/checkbox/ifx-checkbox-s-checked_candidate.png",
         backgroundColor: "#ffffff"
-    }
+    },
+    {
+        name: "ifx-checkbox-m-unchecked-disabled",
+        baseline: "/images/checkbox/ifx-checkbox-m-unchecked-disabled_baseline.png",
+        implementation: "/images/checkbox/ifx-checkbox-m-unchecked-disabled_candidate.png",
+        backgroundColor: "#ffffff"
+    },
+    {
+        name: "ifx-checkbox-s-unchecked-error",
+        baseline: "/images/checkbox/ifx-checkbox-s-unchecked-error_baseline.png",
+        implementation: "/images/checkbox/ifx-checkbox-s-unchecked-error_candidate.png", // TODO: Missing
+        backgroundColor: "#ffffff"
+    },
 ]
 
 function invertColor(hex) {
@@ -137,34 +149,68 @@ const surveyJson = {
                                 <span class="sv-string-viewer" style="font-weight: 600">Please use the slider below to compare the images.</strong>
                             </div>
                             <img-comparison-slider>
-                                <img slot="first" src="${comparison.baseline}" />
-                                <img slot="second" src="${comparison.implementation}" />
+                                <img slot="first" src="${comparison.implementation}" />
+                                <img slot="second" src="${comparison.baseline}" />
                             </img-comparison-slider>
                             <div style="width: 100%; display: flex; justify-content: space-between; margin-top: 10px;">
-                                <p>Design</p>
                                 <p>Implementation</p>
+                                <p>Design</p>
                             </div>
                         </div>
                     `
                 },
                 {
-                    name: "image1",
+                    name: `${comparison.name}-rating`,
                     title: "Is this implementation visually consistent with the design?",
                     rateValues: [
-                        { value: 1, text: "Strong Yes" },
-                        { value: 2, text: "Yes" },
+                        { value: 1, text: "Strong No" },
+                        { value: 2, text: "No" },
                         { value: 3, text: "Unsure" },
-                        { value: 4, text: "No" },
-                        { value: 5, text: "Strong No" }
+                        { value: 4, text: "Yes" },
+                        { value: 5, text: "Strong Yes" }
                     ],
                     type: "rating",
                     rateType: "labels",
                     isRequired: true,
                     requiredErrorText: "Please submit an answer."
+                },
+                {
+                    name: `${comparison.name}-confidence`,
+                    title: "How confident are you in your answer?",
+                    rateValues: [
+                        { value: 1, text: "Not confident" },
+                        { value: 2, text: "Medium" },
+                        { value: 3, text: "Very confident" },
+                    ],
+                    type: "rating",
+                    rateType: "labels",
+                    isRequired: true,
+                    requiredErrorText: "Please submit an answer."
+                },
+                {
+                    name: `${comparison.name}-comments`,
+                    title: "Any additional comments?",
+                    type: "comment",
+                    isRequired: false
                 }
             
             ],
-        }))
+        })),
+        {
+            name: "reportIntro",
+            title: "Almost done!",
+            elements: [
+                {
+                    name: "introduction",
+                    type: "html",
+                    html: `
+                        
+                    `,
+                    isRequired: true,
+                    requiredErrorText: "Please submit an answer."
+                }
+            ]
+        },
     ],
     firstPageIsStartPage: true,
     showNavigationButtons: true,
